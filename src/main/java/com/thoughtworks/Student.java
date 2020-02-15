9885ef3f15238f715b7e94e5e4ef7ee199dcadcc
GraceeFlower <1491644103@qq.com>
2020-02-15 16:55:04 +0800

    [WM] feat: add the Student class

---
+++ src/main/java/com/thoughtworks/Student.java
@@ -0,0 +1,48 @@
+package com.thoughtworks;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Student {
+
+    private String name;
+    private int studentId;
+    private String admissionDate;
+
+    public Student() {
+    }
+
+    public Student(String name, int studentId, String admissionDate) {
+        this.name = name;
+        this.studentId = studentId;
+        this.admissionDate = admissionDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getAdmissionDate() throws ParseException {
+        SimpleDateFormat rowFormat = new SimpleDateFormat("yyyy.MM.dd");
+        Date rowDate = rowFormat.parse(admissionDate);
+        SimpleDateFormat newFormat = new SimpleDateFormat("yyyy年MM月dd日");
+        return newFormat.format(rowDate);
+    }
+
+    public void setAdmissionDate(String admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+}
