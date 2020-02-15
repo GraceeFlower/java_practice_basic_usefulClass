256fa4535dacae720cd9a60c9289a93f3e69f3fd
GraceeFlower <1491644103@qq.com>
2020-02-15 19:19:08 +0800

    [WM] feat: add the new class Teacher & check the repeat students info

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -10,6 +10,7 @@ public class App {
         stuList.add(new Student("张三", 1, "2019.02.11"));
         stuList.add(new Student("李四", 2, "2019.02.10"));
         stuList.add(new Student("王五", 1, "2019.02.09"));
+        stuList.add(new Student("小六", 2, "2019.02.08"));
 
         for (Student stu: stuList) {
             System.out.println("我叫" + stu.getName()
@@ -18,5 +19,7 @@ public class App {
                 + stu.getStudyAge());
         }
 
+        Teacher teacher = new Teacher("张龙", stuList);
+        teacher.checkRepeat();
     }
 }
---
+++ src/main/java/com/thoughtworks/Teacher.java
@@ -0,0 +1,53 @@
+package com.thoughtworks;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Teacher {
+
+    private String name;
+    private ArrayList<Student> stuList;
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, ArrayList<Student> stuList) {
+        this.name = name;
+        this.stuList = stuList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Student> getStuList() {
+        return stuList;
+    }
+
+    public void setStuList(ArrayList<Student> stuList) {
+        this.stuList = stuList;
+    }
+
+    public void checkRepeat() throws ParseException {
+        System.out.println("\n" + this.name + "老师的重复学生信息：");
+        ArrayList<String> idList = new ArrayList<>();
+        for (Student stu: stuList) {
+            idList.add(stu.getStudentId());
+        }
+        HashSet<String> idSet = new HashSet<>();
+        for (int i = 0; i < idList.size(); i++) {
+            if (i != idList.indexOf(idList.get(i))) {
+                idSet.add(idList.get(i));
+            }
+        }
+        for (Student i: stuList) {
+            if (idSet.contains(i.getStudentId())) {
+                System.out.println("姓名：" + i.getName() + "，"
+                    + "学号：" + i.getStudentId() + "，"
+                    + "入学时间：" + i.getAdmissionDate() + "，"
+                    + "学龄：" + i.getStudyAge());
+            }
+        }
+    }
+}
