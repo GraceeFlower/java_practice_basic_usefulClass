aa548e7dece3675aeb37cd3905c48adebb117ca7
GraceeFlower <1491644103@qq.com>
2020-02-17 20:07:23 +0800

    [WM] refactor: delete the unexpected student

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -10,7 +10,6 @@ public class App {
         stuList.add(new Student("张三", 1, "2019.02.11"));
         stuList.add(new Student("李四", 2, "2019.02.10"));
         stuList.add(new Student("王五", 1, "2019.02.09"));
-        stuList.add(new Student("小六", 2, "2019.02.08"));
 
         for (Student stu: stuList) {
             System.out.println("我叫" + stu.getName()
